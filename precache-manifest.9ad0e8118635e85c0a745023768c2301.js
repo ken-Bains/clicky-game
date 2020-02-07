a61d919d90bf33bf7057f57296228a8221d7460f
KB <nkbains10@gmail.com>
2020-02-07 09:42:51 -0800

    Updates

---
+++ precache-manifest.9ad0e8118635e85c0a745023768c2301.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c8dfad9b592418dfa819883ff10bc6d",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "51df2715290a8cc0d034",
+    "url": "/clicky-game/static/css/main.a08823f9.chunk.css"
+  },
+  {
+    "revision": "bc5e42eb7f5dd25e7f41",
+    "url": "/clicky-game/static/js/2.065b170a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/clicky-game/static/js/2.065b170a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51df2715290a8cc0d034",
+    "url": "/clicky-game/static/js/main.00967455.chunk.js"
+  },
+  {
+    "revision": "dce93861a1d9590743d1",
+    "url": "/clicky-game/static/js/runtime-main.0ac4616a.js"
+  }
+]);
